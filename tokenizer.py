4e15333b38f81048a0d0e56365c6b7cd6445de47
FoxNerdSaysMoo <zceboys@gmail.com>
2020-07-14 12:36:19 -0500

    Added comment support

--- tokenizer.py
+++ tokenizer.py
@@ -5,17 +5,35 @@ class Tokenizer:
     @staticmethod
     def tokenize(msfile: str):
         keywords = {
-            'if': tokens.IF,
+            'if': tokens.IF_STATEMENT,
+            'el-if': tokens.IF_STATEMENT,
+            'else': tokens.IF_STATEMENT,
             'import': tokens.IMPORT,
             'loop': tokens.LOOP,
-            'function': tokens.FUNCTION
-        }
+            'function': tokens.FUNCTION,
+            'func': tokens.FUNCTION,
+            'raise': tokens.Error,
+            'error': tokens.Error
+            }
 
     @staticmethod
     def format(msfile: str):
         # Get lines from file.
         with open(msfile, 'r') as file:
             lines = file.readlines()
+
+        # Remove comments from lines
+        for line in lines:
+            if '#' in line:
+                lines[line] = line[:line.find('#')]
+
+        is_comment = False
+        for line in lines:
+            if '///' in line:
+                lines[line] = line[line.find('///'):]
+                is_comment = -is_comment
+            elif is_comment:
+                lines[line] = ''
 
         # Remove all empty lines from the list
         while '\n' in lines:
