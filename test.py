1f6cd4d3f2dcada5b04b238f94610ca39c37ef90
FoxNerdSaysMoo <zceboys@gmail.com>
2020-07-15 10:33:51 -0500

    Minor Tweaks

--- test.py
+++ test.py
@@ -2,11 +2,12 @@ from tokenizer import Tokenizer
 import pprint
 from errors import Error
 
+
 interpreter = Tokenizer()
 
 pprint.pprint(interpreter.format('mantis.ms'))
 
-error = Error('SyntaxError',
+error = Error('ByeeeeeError',
               {'test.py': 'line 9', 'error.py': 'line 18'},
               ['line 9: error = Error()', 'line 18: sys.exit()'],
               traceback='byeeee')
